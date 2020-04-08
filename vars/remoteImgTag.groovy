14eda740167d493a0cbe0fc5f888d46c5dfe7677
hongkisung <kshong0531@sk.com>
2020-04-08 11:42:40 +0900

    modify test

--- vars/remoteImgTag.groovy
+++ vars/remoteImgTag.groovy
@@ -1,59 +1,100 @@
 import retort.utils.logging.Logger
 import retort.exception.RetortException
 import static retort.utils.Utils.delegateParameters as getParam
+//dockerCmd.remoteTag from: 'my/image:dev-1', to: 'my/image:prd-1', registry: HARBOR_REGISTRY, credentialsId: 'DOCKER_CREDENTIALS'
+//ockerCmd.remoteTag fromImage: 'my/image, fromVersion: dev-1', toImage: 'my/image, toVersion: 'prd-1', registry: HARBOR_REGISTRY, credentialsId: 'DOCKER_CREDENTIALS'
 
-def push(ret) {
+def remoteTag(ret) {
     Logger logger = Logger.getLogger(this)
     logger.info "stared remote image change tag and push"
 
     def config = getParam(ret)
 
     if(retParamErrorCheck(config, logger)) {
-        def token = getToken(config, logger)
+        def remotImgMap = convertNewMap(config, logger)
+        def token = getToken(remotImgMap, logger)
         if(token) {
-            def metaData = getManifestPath(token, config, logger)
+
+            def metaData = getManifestPath(token, remotImgMap, logger)
             if(metaData){
-                pushImgNew(token, metaData, config, logger)
+                pushImgNew(token, metaData, remotImgMap, logger)
             }
         }
     }
 }
 
+
 private def retParamErrorCheck(config, logger) {
     logger.info "starting request param error check"
     
-    if (!config.imageName) {
-        logger.error("imageName is required.")
-        throw new RetortException('RC203')
+    if (!config.registry) {
+        logger.error("registry is required.")
+        throw new RetortException('RC213')
+    }
+        
+    if(!config.from) {
+        logger.error("from: projectName/ImageName:tag values is required.")
+        throw new RetortException('RC211')
     }
 
-    if (!config.imageOldVersion) {
-        logger.error("imageOldVersion is required.")
-        throw new RetortException('RC105')
+    if(!config.to) {
+         logger.error("to: projectName/ImageName:tag values is required.")
+        throw new RetortException('RC212')
     }
 
-    if (!config.imageNewVersion) {
-        logger.error("imageNewVersion is required.")
-        throw new RetortException('RC106')
-    }
     return true
 }
 
+
+private def convertNewMap(config, logger) {
+    logger.info "stared convertNewMap"
+
+    def remoteImgMap = [:]
+
+    config.each { key, value ->
+        if('from'.equals(key)) {
+            def fromArrValue= value.split(':')
+            if(fromArrValue.length == 2) {
+                remoteImgMap.put('fromImage',fromArrValue[0])
+                remoteImgMap.put('fromVersion',fromArrValue[1])
+            }else {
+                logger.error 'Please set from: projectname/imagename:tagversion ex) from: myproject/myimage:dev-1'
+                throw new RetortException('RC211')
+            }
+        }else if('to'.equals(key)) {
+            def toArrValue = value.split(':')
+            def toArrValue= value.split(':')
+            if(toArrValue.length == 2) {
+                remoteImgMap.put('toImage',toArrValue[0])
+                remoteImgMap.put('toVersion',toArrValue[1])
+            }else {
+                logger.error 'Please set to: projectname/imagename:tagversion ex) to: myproject/myimage:prd-1'
+                throw new RetortException('RC212')
+            }
+        }else {
+            remoteImgMap.put(key,value)
+        }
+    }
+    logger.debug "remoteImgMap : ${remoteImgMap}"
+    return remoteImgMap;
+}
+
+
 // dockerCmd.push registry: HARBOR_REGISTRY, imageName: DOCKER_IMAGE, imageOldVersion: OLD_VERSION, imageNewVersion: NEW_VERSION, credentialsId: "HARBOR_CREDENTIALS"
 
-private def getToken(config, logger) {
-    logger.info "starting get token Logging in image registry with ${config.credentialsId}"
+private def getToken(remotImgMap, logger) {
+    logger.info "starting get token Logging in image registry with ${remotImgMap.credentialsId}"
 
     def TOKEN_PATH = '/service/token?service=harbor-registry&scope=repository:'
     StringBuffer tokenUrl = new StringBuffer("https://")
 
-    if(config.registry) {
-        tokenUrl.append("${config.registry}")
+    if(remotImgMap.registry) {
+        tokenUrl.append("${remotImgMap.registry}")
         tokenUrl.append(TOKEN_PATH)
     }
 
-    if(config.imageName) {
-        tokenUrl.append("${config.imageName}:pull,push")
+    if(remotImgMap.fromImage) {
+        tokenUrl.append("${remotImgMap.fromImage}:pull,push")
     }
 
     echo tokenUrl.toString()
@@ -83,20 +124,20 @@ private def getToken(config, logger) {
     return token
 }
 
-private def getManifestPath(token, config, logger) {
-    logger.info "starting get manifestInfo in image registry with registry : ${config.registry}, imageName : ${config.imageName}, imageOldVersion : ${config.imageOldVersion}"
-    def MANIFEST_TYPE = "/v2/" + config.imageName + "/manifests/"
+private def getManifestPath(token, remotImgMap, logger) {
+    logger.info "starting get manifestInfo in image registry with - [ registry: ${remotImgMap.registry}, FromImage : ${remotImgMap.imageName}, tagFromVersion : ${remotImgMap.fromVersion} ]"
+    def MANIFEST_TYPE = "/v2/" + remotImgMap.fromImage + "/manifests/"
     def con_type = "application/vnd.docker.distribution.manifest.v2+json"
     StringBuffer getManifestUrl = new StringBuffer("https://")
     
-    if(config.registry) {
-        getManifestUrl.append("${config.registry}")
+    if(remotImgMap.registry) {
+        getManifestUrl.append("${remotImgMap.registry}")
     }
 
     getManifestUrl.append(MANIFEST_TYPE)
 
-    if(config.imageOldVersion) {
-        getManifestUrl.append("${config.imageOldVersion}")
+    if(remotImgMap.fromVersion) {
+        getManifestUrl.append("${remotImgMap.fromVersion}")
     }
 
     echo getManifestUrl.toString()
@@ -124,20 +165,21 @@ private def getManifestPath(token, config, logger) {
     return responseBody.content
 }
 
-private void pushImgNew(token, metaData, config, logger) {
-    logger.info "starting get manifestInfo in image registry with registry : ${config.registry}, imageName : ${config.imageName}, imageNewVersion : ${config.imageNewVersion}"
-    def MANIFEST_TYPE = "/v2/" + config.imageName + "/manifests/"
+private void pushImgNew(token, metaData, remotImgMap, logger) {
+    logger.info "starting get manifestInfo in image registry with - [registry: ${remotImgMap.registry}, toImage : ${remotImgMap.toImage}, tagToVersion : ${remotImgMap.toVersion} ]"
+    
+    def MANIFEST_TYPE = "/v2/" + remotImgMap.toImage + "/manifests/"
     def con_type = "application/vnd.docker.distribution.manifest.v2+json"
     StringBuffer getManifestUrl = new StringBuffer("https://")
     
-    if(config.registry) {
-        getManifestUrl.append("${config.registry}")
+    if(remotImgMap.registry) {
+        getManifestUrl.append("${remotImgMap.registry}")
     }
     
     getManifestUrl.append(MANIFEST_TYPE)
 
-    if(config.imageNewVersion) {
-        getManifestUrl.append("${config.imageNewVersion}")
+    if(remotImgMap.toVersion) {
+        getManifestUrl.append("${remotImgMap.toVersion}")
     }
 
     def responseBody
